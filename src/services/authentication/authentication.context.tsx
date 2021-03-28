a571af5b117d35814508f8979b23e68de3661050
Jorge Contreras <jorge.contreras789@gmail.com>
2021-03-27 21:33:51 -0400

    Updated services/authentication files to ts

--- src/services/authentication/authentication.context.tsx
+++ src/services/authentication/authentication.context.tsx
@@ -1,14 +1,17 @@
 import React, { useState, createContext } from "react";
 import * as firebase from "firebase";
+import { UserInfo } from "firebase";
 
 import { loginRequest } from "./authentication.service";
 
-export const AuthenticationContext = createContext();
+export const AuthenticationContext = createContext({});
 
-export const AuthenticationContextProvider = ({ children }) => {
+export const AuthenticationContextProvider: React.FC<any> = ({ children }) => {
   const [isLoading, setIsLoading] = useState(false);
-  const [user, setUser] = useState(null);
-  const [error, setError] = useState(null);
+  const [user, setUser] = useState<
+    firebase.User | firebase.auth.UserCredential | null
+  >(null);
+  const [error, setError] = useState<string | null>(null);
 
   firebase.auth().onAuthStateChanged((usr) => {
     if (usr) {
@@ -19,7 +22,7 @@ export const AuthenticationContextProvider = ({ children }) => {
     }
   });
 
-  const onLogin = (email, password) => {
+  const onLogin = (email: string, password: string) => {
     setIsLoading(true);
     loginRequest(email, password)
       .then((u) => {
@@ -32,7 +35,11 @@ export const AuthenticationContextProvider = ({ children }) => {
       });
   };
 
-  const onRegister = (email, password, repeatedPassword) => {
+  const onRegister = (
+    email: string,
+    password: string,
+    repeatedPassword: string
+  ) => {
     setIsLoading(true);
     if (password !== repeatedPassword) {
       setError("Error: Passwords do not match");
--- src/services/authentication/authentication.service.ts
+++ src/services/authentication/authentication.service.ts
@@ -1,4 +1,4 @@
 import * as firebase from "firebase";
 
-export const loginRequest = (email, password) =>
+export const loginRequest = (email: string, password: string) =>
   firebase.auth().signInWithEmailAndPassword(email, password);
